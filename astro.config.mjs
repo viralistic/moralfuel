6480b7a0da32411e15b5351382d31f2448621276
viralistic <info@viralistic.nl>
2025-01-15 11:59:21 +0100

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+import { defineConfig } from 'astro/config'
+import fulldev from 'fulldev-ui/integration'
+
+export default defineConfig({
+  integrations: [fulldev()],
+})
